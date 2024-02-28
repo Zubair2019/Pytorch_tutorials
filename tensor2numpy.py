b5de39e21373effcfba5f8e036c87dfe711d5cf3
Zubair Khaliq <zikayem@gmail.com>
2024-02-28 15:18:37 +0530

    add data

--- tensor2numpy.py
+++ tensor2numpy.py
@@ -13,3 +13,12 @@ a.add_(2)
 print(a)
 print(x)  # Since both the numpy array and the tensor are on CPU so a change to one of them results in 
             #A CHANGE TO THE OTHER AS WELL
+
+b = numpy.ones(6)
+print(b, type(b))
+
+y = torch.from_numpy(b)
+print(y, type(y))
+
+z = y.add_(1)
+print(z,b) # Similar phenomena but in opposite direction
