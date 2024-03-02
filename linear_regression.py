6f5b65043a855e56a020b9f005dfb1d45eecf2d2
Zubair Khaliq <zikayem@gmail.com>
2024-03-02 11:06:37 +0530

    added linear_regression

---
+++ linear_regression.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from sklearn import datasets
+import matplotlib.pyplot as plt
+# 0. Prepare Data
+X_numpy,y_numpy = datasets.make_regression(n_samples=100, n_features=1,noise=20, random_state=1)
+X = torch.from_numpy(X_numpy.astype(np.float32))
+y = torch.from_numpy(y_numpy.astype(np.float32))
+print(y.shape)
+y = y.view(y.shape[0],1)
+print(y.shape)
+n_samples,n_features = X.shape
+
+# 1. Model
+input_size = n_features
+output_size = 1
+model = nn.Linear(input_size,output_size)
+
+
+# 2. Loss and Optimizer
+learning_rate = 0.1
+criterion = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(),lr= learning_rate)
+
+# 3. Training Loop
+n_epochs = 200
+
+for epoch in range(n_epochs):
+    #forward
+    y_predicted = model(X)
+    loss = criterion(y_predicted,y)
+    #backward pass
+    loss.backward()
+    #weight update
+    optimizer.step()
+    optimizer.zero_grad()
+
+    if (epoch+1 %10 == 0):
+        print(f'epoch : {epoch+1} , loss : {loss.item():.4f}')
+
+#plot
+predicted = model(X).detach().numpy()
+plt.plot(X_numpy,y_numpy, 'ro')
+plt.plot(X_numpy,predicted, 'b')
+plt.show()
